7cf992309fcb4cb87a17fe08727d5231db439d11
William Insignare <ingenieroinsignares@hotmail.com>
2021-03-20 16:46:35 -0500

    cabio 20032021

---
+++ frontend/pagina2.php
@@ -0,0 +1,13 @@
+<?php
+    require_once('Componentes/head.php');
+?>
+  <body class="text-center">
+    
+      <h1>
+        Hola estamos en la segunda pagina
+      </h1>
+
+
+    
+  </body>
+</html>
